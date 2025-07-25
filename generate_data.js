9edc89da824987431e80b161017cdb72fdbac8cd
konichiwaku <andimaspoetra2@gmail.com>
2025-07-25 19:49:34 +0700

    Create generate_data.js

---
+++ generate_data.js
@@ -0,0 +1,12 @@
+{
+  "name": "kucoin-data-generator",
+  "version": "1.0.0",
+  "description": "Script to fetch KuCoin data for multiple coins and generate JSON for R2",
+  "main": "generate_data.js",
+  "scripts": {
+    "start": "node generate_data.js"
+  },
+  "dependencies": {
+    "node-fetch": "^2.6.7"
+  }
+}
